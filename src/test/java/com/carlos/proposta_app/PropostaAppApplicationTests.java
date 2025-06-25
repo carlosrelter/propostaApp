ec90ef63f93cf2c4881d1bab3d426e49e13bb1d1
carlosrelter <reltercontato@gmail.com>
2025-06-25 16:37:12 -0300

    first commit

---
+++ src/test/java/com/carlos/proposta_app/PropostaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
