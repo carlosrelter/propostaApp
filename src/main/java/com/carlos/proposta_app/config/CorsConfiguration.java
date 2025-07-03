dd5e4b67d40c0065d25862d99cc994997553e73b
carlosrelter <reltercontato@gmail.com>
2025-07-03 07:26:41 -0300

    configuracoes do rabbit

--- src/main/java/com/carlos/proposta_app/config/CorsConfiguration.java
+++ src/main/java/com/carlos/proposta_app/config/CorsConfiguration.java
@@ -10,7 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost/")
-                .allowedMethods("*");
+                .allowedOrigins("http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
